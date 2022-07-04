9894a5b2cfe23049074caf0e91f9b287c1639897
zhoudedi <64512474+zhoudedi@users.noreply.github.com>
2022-07-05 02:15:27 +0800

    Update herWeater.py

--- herWeater.py
+++ herWeater.py
@@ -28,7 +28,7 @@ def ServerPush(info): #Server酱推送
 def CoolPush(info): #CoolPush酷推
     # cpurl = 'https://push.xuthus.cc/group/'+spkey   #推送到QQ群
     # cpurl = 'https://push.xuthus.cc/send/' + SKey  # 推送到个人QQ
-    api='https://push.xuthus.cc/send/{}'.format(SKey)
+    api='https://push.xuthus.cc/psend/{}'.format(SKey)
     print(api)
     print(info)
     requests.post(api, info.encode('utf-8'))
