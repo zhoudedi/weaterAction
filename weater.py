2cf4c891e6434599dfaba8ed6116cd9c5fa26016
zhoudedi <64512474+zhoudedi@users.noreply.github.com>
2022-07-05 01:48:41 +0800

    Update weater.py

--- weater.py
+++ weater.py
@@ -27,8 +27,8 @@ def ServerPush(info): #Server酱推送
     requests.post(api, data=data)
 def CoolPush(info): #CoolPush酷推
     # cpurl = 'https://push.xuthus.cc/group/'+spkey   #推送到QQ群
-    # cpurl = 'https://push.xuthus.cc/send/' + SKey  # 推送到个人QQ
-    api='https://push.xuthus.cc/send/{}'.format(SKey)
+    # cpurl = 'https://push.xuthus.cc/send/' + SKey  # 推送到个人QQhttps://push.xuthus.cc/psend/e8dd17c722b284e14092c082fee0ee09
+    api='https://push.xuthus.cc/psend/{}'.format(SKey)
     print(api)
     print(info)
     requests.post(api, info.encode('utf-8'))
