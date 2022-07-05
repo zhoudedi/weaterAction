dbdd37cc5db7fdad41c6e50d8f5211fc5d1b6909
zhoudedi <64512474+zhoudedi@users.noreply.github.com>
2022-07-05 13:19:29 +0800

    Update tqtsqqq.py

--- tqtsqqq.py
+++ tqtsqqq.py
@@ -61,7 +61,7 @@ def main():
             tips = d["data"]["forecast"][0]["notice"] #温馨提示
             # 天气提示内容
             tdwt = "【今日❤️天气】\n城市： " + parent + city + \
-                   "\n日期：" + date + "爱你鑫鑫😘"\n星期: " + week + "\n天气: " + weather_type + "\n温度: " + wendu_high + " / "+ wendu_low + "\n湿度: " + \
+                   "\n日期：" + date + "爱你鑫鑫😘" + "\n星期: " + week + "\n天气: " + weather_type + "\n温度: " + wendu_high + " / "+ wendu_low + "\n湿度: " + \
                     shidu + "\nPM25: " + pm25 + "\nPM10: " + pm10 + "\n空气质量: " + quality + \
                    "\n风力风向: " + fx + fl + "\n感冒指数: "  + ganmao + "\n温馨提示： " + tips + "\n更新时间: " + update_time + "\n✁-----------------------------------------\n" + get_iciba_everyday()
             # print(tdwt)
